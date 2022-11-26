8dd951071e4ad82d392a9bc97af0f2d16acbf89f
PrinceJetro <adegbuyijephthah@gmail.com>
2022-11-26 01:38:16 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function showVideo(){
+    document.getElementById("video-container").style.display="block";
+    document.getElementById("main").style.display="none";
+
+}
+
+function c(){
+    location.reload()
+    
+}
